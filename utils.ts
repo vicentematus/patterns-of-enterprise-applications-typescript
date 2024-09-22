0444bc48e56ad9f22b08e9f1ea36ba80a5056261
Vicente Matus <38754555+vicentematus@users.noreply.github.com>
2024-09-22 18:35:59 -0300

    add config things

---
+++ utils.ts
@@ -0,0 +1,5 @@
+// Colors for console.log natively
+// ANSI Code or something like that??
+// https://stackoverflow.com/a/41407246
+export const GREEN_TEXT = "\x1b[32m%s\x1b[0m";
+export const RED_TEXT = "\x1b[31m%s\x1b[0m";
