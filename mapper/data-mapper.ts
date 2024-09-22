6a5fe6fefe91a1a60c3968894b4215fd6eed6d61
Vicente Matus <38754555+vicentematus@users.noreply.github.com>
2024-09-22 18:35:26 -0300

    add mapper pattern

---
+++ mapper/data-mapper.ts
@@ -0,0 +1,6 @@
+import { db } from "../db-initialize";
+class DataMapper {
+  find() {}
+}
+
+class Person {}
---
+++ mapper/mapper.ts
@@ -0,0 +1,10 @@
+// # Data Mapper
+// A layer of mappers that moves data between objects and a database while keeping them independent of each other and the mapper itself.
+// https://martinfowler.com/eaaCatalog/dataMapper.html
+
+import { db } from "../db-initialize";
+class DataMapper {
+  find() {}
+}
+
+class Person {}
